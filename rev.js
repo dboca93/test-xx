cee7989cfde3f0ccb40a472f8891a21449162a17
dboca93 <78838162+dboca93@users.noreply.github.com>
2022-05-17 15:29:20 +1000

    3C

---
+++ rev.js
@@ -0,0 +1,15 @@
+//This document is meant as merely a review document for the objects section 
+//of the codeacademy course. 
+
+let mercedes = {
+    color: 'black',
+}
+console.log(mercedes.color); 
+
+let redHouse = {
+    'street number':  18, 
+    name: 'The Red House', 
+}
+console.log(redHouse['street number']); 
+
+let propertyPlease = 'street number'
